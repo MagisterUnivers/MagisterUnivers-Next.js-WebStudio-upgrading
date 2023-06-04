1a537bb658410a3ab44e753cf1984330fe2d2b83
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-06-04 20:38:29 +0300

    ADD: Meta correction and lang

--- webstudio-upgrade/pages/_document.js
+++ webstudio-upgrade/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
 	return (
-		<Html lang="en">
+		<Html lang="uk">
 			<Head>
 				<meta name="description" content="WebStudio website portfolio" />
 				<meta name="googlebot" content="index, follow" />
@@ -12,7 +12,7 @@ export default function Document() {
 					name="keywords"
 					content="Webstudio, WEBSTUDIO, webstudio, Studio, studio, STUDIO"
 				/>
-				<meta name="msapplication-TileColor" content="#da532c" />
+				<meta name="application-TileColor" content="#2196f3" />
 				<meta name="theme-color" content="#ffffff" />
 				<meta name="author" content="Team Undefined - MagisterUnivers" />
 				<link href="/favicon-custom.ico" rel="icon" type="image/x-icon" />
